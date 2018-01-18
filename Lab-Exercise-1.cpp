7efece288e191180afbda378853826c150360197
p44n <33915695+p44n@users.noreply.github.com>
2018-01-18 10:04:55 +0800

    try run
    done

--- Lab-Exercise-1.cpp
+++ Lab-Exercise-1.cpp
@@ -6,20 +6,30 @@ using namespace std;
 int main()
 {
 
-	int no1,no2,x,sum;
+	int no1,no2,r,sum;
 
+	
+	
+	
 	cout<<"Find the sum of a given number"<<endl;
 	cout<<"------------------------------"<<endl;
-
 	cout<<"Input number: "<<endl;
+	cin >> no1;
+	no2 = no1;
+	
+	
 
 
-	while(no2>0)
+	while(no1 > 0)
 	{
 	r=no1%10;
 	no1=no1/10;
-	sum=sum+x;
+	sum=sum+r;
 
 	}
+	
 	//print the sum of the digits here.
+	cout << "Sum of digit of " << no2 << " is:" << sum << endl;
+	
 }
+
