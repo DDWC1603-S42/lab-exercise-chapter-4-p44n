2ed038e10723c6d34097667e04f578ca4206527e
p44n <33915695+p44n@users.noreply.github.com>
2018-01-18 09:36:29 +0800

    try run
    done

--- Lab-Exercise-2.cpp
+++ Lab-Exercise-2.cpp
@@ -7,27 +7,36 @@ using namespace std;
 int main()
 {
 
+	cout << "\n\n Find the perfect numbers between number 1 and 500:\n";
+	cout << "-------------------------------------------------------\n";
 	int i=1,u=1,sum=0;
-	cout<<"The perfect numbers between 1 to 500 are: "<<endl;
-
-	while(i<=500)
-
+	cout<<"The perfect numbers between 1 to 500 are: \n"<<endl;
+    while(i<=500)
+    {
+	
 		while(u<=500)
-
+     {
+	 
 			if(u<i)
-
-				if(i%u=)
-					sum=sum+u;
-
-	u++
-
-		if(sum=i)
+			{
 			
-			cout<<i<<" "<<endl;
 
-	i++
+				if(i % u ==0)
+					sum=sum+u;
+				}
+
+	u++;
+}
+		if(sum==i) {
+		
+		cout<<i<<" "<<"\n";
+		}
+			
+			
+
+	i++;
 		u=1;
 		sum=0;
 
-
+}
 }
